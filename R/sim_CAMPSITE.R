9aedbfd850096d62629b46ad2819e0f149e7b586
Anna Krystalli <annakrystalli@googlemail.com>
2022-02-15 11:13:17 +0000

    re-arrange into package structure

--- R/sim_CAMPSITE.R
+++ R/sim_CAMPSITE.R
@@ -388,3 +388,46 @@ plot_CAMPSITE <- function(traitmat, linmat, step_size, ylims = NULL) {
     }
   }
 }
+
+
+
+runCAMPSITE <- function(comp, selec){
+  source("code/sim_CAMPSITE.R", local = T)
+  cores = detectCores()
+  cl <- makeCluster(cores[1]-1) #not to overload your computer
+  registerDoParallel(cl)
+  set.seed(42)
+  sim <- foreach(i=1:100, .packages = c("ape")) %dopar% {
+    repeat {
+      model <- sim_CAMPSITE(pars = c(0.25,
+                                     0.01,
+                                     0.6,
+                                     0.2,
+                                     0.2,
+                                     0.01,
+                                     0.4,
+                                     0.4,
+                                     0.02,
+                                     comp,
+                                     comp,
+                                     selec,
+                                     0.5,
+                                     20),
+                            ou = list(c(0),
+                                      c(selec)),
+                            bounds = c(-Inf,
+                                       Inf),
+                            root.value = 0,
+                            age.max = 50,
+                            step.size = 0.01,
+                            full.sim = T,
+                            plot = F
+      )
+      if (!isTRUE(model$gsp_extant$tree == c("process died")) && model$gsp_extant$tree$Nnode > 4) {break}
+    }
+    model
+  }
+  stopCluster(cl)
+  saveRDS(sim, paste("output/sims/comp", comp, "_selec", selec, ".rds", sep = ""))
+  rm(sim)
+}
